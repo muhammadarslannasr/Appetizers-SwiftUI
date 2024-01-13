a2b3a446e40d1ae2f3c19ae080b61fec5e7e4082
arslan <iamarslannasr@gmail.com>
2024-01-13 10:56:31 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Muhammad Arslan Nasr on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
