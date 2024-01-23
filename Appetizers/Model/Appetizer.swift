0067b394e3d0599da705649def9eb1b738602c8c
Arslan <arslan.nasr@rnssol.com>
2024-01-23 15:35:24 +0500

    on delete flow added and order view is done

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -27,4 +27,15 @@ class MockData{
                                            imageURL: "", price: 9.99, calories: 99, protein: 99, carbs: 99)
     
     static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    
+    static let orderItemOne = Appetizer(id: 0001, name: "Test Appetizer One", description: "This is the description of my appetizer, and its yummy",
+                                           imageURL: "", price: 9.99, calories: 99, protein: 99, carbs: 99)
+    
+    static let orderItemTwo = Appetizer(id: 0002, name: "Test Appetizer Two", description: "This is the description of my appetizer, and its yummy",
+                                           imageURL: "", price: 9.99, calories: 99, protein: 99, carbs: 99)
+    
+    static let orderItemThree = Appetizer(id: 0003, name: "Test Appetizer Three", description: "This is the description of my appetizer, and its yummy",
+                                           imageURL: "", price: 9.99, calories: 99, protein: 99, carbs: 99)
+    
+    static let orderItems = [orderItemOne, orderItemTwo, orderItemThree]
 }
--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct OrderView: View {
+    @State private var orderItems = MockData.orderItems
     var body: some View {
         NavigationView{
-            Text("Order View")
-                .navigationTitle("🧾 Appetizers")
+            VStack{
+                List{
+                    ForEach(orderItems){ appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
+                
+                Button{
+                    print("order placed")
+                }label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+                .navigationTitle("🧾 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet){
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
